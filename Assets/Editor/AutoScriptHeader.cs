db0cfc23e41f411db80f1ecd77c33bf85cde1dd6
TigerPapaDev <mto.s@163.com>
2018-02-08 13:26:43 +0800

    second commit

---
+++ Assets/Editor/AutoScriptHeader.cs
@@ -0,0 +1,36 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace BioVR
+{
+    /// <summary>
+    /// 新建脚本自动添加头部注释
+    /// </summary>
+    public class AutoScriptHeader : UnityEditor.AssetModificationProcessor
+    {
+        public static void OnWillCreateAsset(string newFileMeta)
+        {
+            string newFilePath = newFileMeta.Replace(".meta", "");
+            string fileExt = Path.GetExtension(newFilePath);
+            if (fileExt != ".cs")
+            {
+                return;
+            }
+            //注意，Application.datapath会根据使用平台不同而不同  
+            string realPath = Application.dataPath.Replace("Assets", "") + newFilePath;
+            string scriptContent = File.ReadAllText(realPath);
+
+            //这里实现自定义的一些规则  
+            scriptContent = scriptContent.Replace("#SCRIPTFULLNAME#", Path.GetFileName(newFilePath));
+            scriptContent = scriptContent.Replace("#PROJECTNAME#", PlayerSettings.productName);
+            scriptContent = scriptContent.Replace("#COMPANY#", PlayerSettings.companyName);
+            scriptContent = scriptContent.Replace("#AUTHOR#", "Roy");
+            scriptContent = scriptContent.Replace("#VERSION#", "1.0");
+            scriptContent = scriptContent.Replace("#UNITYVERSION#", Application.unityVersion);
+            scriptContent = scriptContent.Replace("#DATE#", System.DateTime.Now.ToString("yyyy-MM-dd"));
+
+            File.WriteAllText(realPath, scriptContent);
+        }
+    }
+}
---
+++ Assets/_Scripts/NewBehaviourScript.cs
@@ -0,0 +1,26 @@
+/** 
+ *Copyright(C) 2015 by DefaultCompany 
+ *All rights reserved. 
+ *FileName:     NewBehaviourScript.cs 
+ *Author:       Roy 
+ *Version:      1.0 
+ *UnityVersion：5.6.4f1 
+ *Date:         2018-02-08 
+ *Description:    
+ *History: 
+*/  
+using UnityEngine;  
+using System.Collections;  
+  
+public class NewBehaviourScript : MonoBehaviour {  
+  
+    // Use this for initialization  
+    void Start () {  
+      
+    }  
+      
+    // Update is called once per frame  
+    void Update () {  
+      
+    }  
+}  
